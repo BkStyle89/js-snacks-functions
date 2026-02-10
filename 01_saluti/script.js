c59fb7355f6aedc852c997303fb47687eee616e7
BkStyle89 <kajzer@hotmail.it>
2026-02-10 16:11:53 +0100

    first exercise

---
+++ 01_saluti/script.js
@@ -0,0 +1,23 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+// Dichiara la funzione qui.
+
+function newString(userName,stringToInsert,position){
+    const result =userName.slice(4, position) + stringToInsert +userName.slice(position);
+    return result
+}
+
+const stringToInsert = "ciao";
+const position = 0;
+const result = newString(userName,stringToInsert,position);
+console.log(result)
+let cut = result[4].split(" ");
+console.log(cut)
+
+// Invoca la funzione qui e stampa il risultato in console
+
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
