c53137b022515b889c58e517070a106bc6240a5c
BkStyle89 <kajzer@hotmail.it>
2026-02-10 17:26:53 +0100

    array

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -9,23 +9,25 @@ function welcome(name){
     return saluto + name
 
 }
-const test = welcome(userName)
-console.log(test)
 
 /* function newString(userName,stringToInsert,position){
     const result =userName.slice(4, position) + stringToInsert +userName.slice(position);
     return result
-}
+    }
+    
+    const stringToInsert = "ciao";
+    const position = 0;
+    const result = newString(userName,stringToInsert,position);
+    console.log(result)
+    let cut = result[4].split(" ");
+    console.log(cut)
+    */
 
-const stringToInsert = "ciao";
-const position = 0;
-const result = newString(userName,stringToInsert,position);
-console.log(result)
-let cut = result[4].split(" ");
-console.log(cut)
- */
+
 // Invoca la funzione qui e stampa il risultato in console
-
+   
+   const test = welcome(userName)
+   console.log(test)
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+ function firstLetter(first_letters){
+    const letter = []
+    for(i=0;i<first_letters.length;i++ ){
+        const char = first_letters[i]
+        console.log(char)
+        letter.push(first_letters)
+} 
+
+return letter
+}
+const letter = firstLetter(names)
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
