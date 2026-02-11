527bdbf0cea8568d5d35b4ada85003b2dbd6e957
BkStyle89 <kajzer@hotmail.it>
2026-02-11 15:29:15 +0100

    exercise 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function firstNames(names_letter){
 
+    const letter =[]
+    
+    for(let i=0;i<names_letter.length;i++){
+        if(names_letter[i].toLowerCase().startsWith('a')){
+        letter.push(names_letter[i])
+        } 
+    }
+    return letter;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = firstNames(names)
+console.log(result)
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
