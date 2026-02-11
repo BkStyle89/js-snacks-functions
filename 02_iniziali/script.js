dd9252a3ad5463123ca405e391afca213af465e1
BkStyle89 <kajzer@hotmail.it>
2026-02-11 15:31:56 +0100

    exercise 2 arrow function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,7 +5,7 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
- function firstLetter(first_letters){
+/*  function firstLetter(first_letters){
     const letter = []
     for(i=0;i<first_letters.length;i++ ){
         const char = first_letters[i][0]
@@ -17,8 +17,19 @@ return letter.join('');
 }
 const result = firstLetter(names);
 console.log(result);
+ */
+const firstLetter = (first_letters) =>{
+    const letter = []
+    for(i=0;i<first_letters.length;i++ ){
+        const char = first_letters[i][0]
+        letter.push(char)
+        
+} 
 
-
+return letter.join('');
+}
+const result = firstLetter(names);
+console.log(result);
 
 
 // Invoca la funzione qui e stampa il risultato in console
