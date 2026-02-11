894146ec71692a16a169a784b7b21b9888dcfdca
BkStyle89 <kajzer@hotmail.it>
2026-02-11 16:06:04 +0100

    exercise 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,22 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vowels(word_vowels){
+
+    const letters=[]
+    const char =['a','e','i','o','u']
+    for(let i=0; i<word_vowels.length;i++){
+        const lettera = word_vowels[i]
+        if(char.indexOf(lettera) != -1){
+            letters.push(lettera)
+        }
+    }
+    return letters
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const result = vowels(word)
+console.log(result.length,result)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
